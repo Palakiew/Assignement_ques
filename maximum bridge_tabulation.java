540d26f27e16cac32782946bb633e5638e172e92
Palak Bhardwaj <112475654+Palakiew@users.noreply.github.com>
2025-06-12 19:32:08 +0530

    maximum bridge_tabulation.java

--- maximum bridge_tabulation.java
+++ maximum bridge_tabulation.java
@@ -4,7 +4,6 @@ class Solution {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (a[i - 1] == b[j - 1]) {
-                    // Characters match, include it in LCS
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
